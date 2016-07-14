40210c0b6bfdb8e2d5d6fa72912fd767790028cb
MarineDruet <druet.marine@gmail.com>
2016-07-14 16:02:15 +0200

    apparence menu

--- app/component/todos/todos.component.ts
+++ app/component/todos/todos.component.ts
@@ -6,6 +6,7 @@ import { ProjectService } from '../project/project.service';
 @Component({
     selector: 'todos',
     templateUrl: 'app/component/todos/todos.component.html',
+    styleUrls: ['app/component/todos/todos.component.css'],
     directives : [ ProjectComponent ],
     providers: [ ProjectService ]
 })
